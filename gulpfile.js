79420dd161a170eb075381cd36a4d1f628f64350
Roman <sosinrd@gmail.com>
2020-03-13 18:42:45 +0300

    Добавил_папку_build_в_неотслеживаемые

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 "use strict";
 
-var gulp = require("gulp"); /* Подключаем gulp, функцией require в nodejs*/
+var gulp = require("gulp"); /* Подключаем gulp, функцией require в node.js*/
 var del = require("del");
 var plumber = require("gulp-plumber"); /* Подключаем инструмент для вывода ошибок в
 нормальную форму при преобразовании процессорного файла в css */
 var sourcemap = require("gulp-sourcemaps"); /* Подключаем для просмотра исходного файла в инструментах разработчика
 в браузере */
-var less = require("gulp-less"); /* Подключаем установленную через npm i gulp-less зависимость */
-var postcss = require("gulp-postcss"); /* Подключаем префиксы для старых браузеров */
-var autoprefixer = require("autoprefixer"); /* Подключаем автопрефиксер для postcss */
+var less = require("gulp-less"); /* Подключаем установленную через npm i gulp-less */
+var postcss = require("gulp-postcss"); /* Подключаем post-css для работы с css файлом */
+var autoprefixer = require("autoprefixer"); /* Подключаем автопрефиксер для postcss префиксы для старых браузеров */
 var server = require("browser-sync").create(); /* Запускаем локальный сервер с помощью browser-sync */
 var csso = require("gulp-csso"); /* Подключаем минимизатор для CSS */
 var rename = require("gulp-rename"); /* Подключаем gulp-rename для переименование файлов */
@@ -20,16 +20,16 @@ var include = require("posthtml-include"); /* Добавляем плагин д
 в определенное место html-документа */
 
 gulp.task("css", function () { /* конвертация препроцессорного кода и минификация CSS */
-  return gulp.src("source/less/style.less")
+  return gulp.src("source/less/style.less") /* Берем контент style.less */
     .pipe(plumber()) /* Запускаем plumber */
     .pipe(sourcemap.init())
-    .pipe(less()) /* Запускаем функцию less */
+    .pipe(less()) /* Преобразовываем less в css */
     .pipe(postcss([ /* Запускаем postcss с внутренним автопрефиксиром */
       autoprefixer()
     ]))
-    .pipe(csso()) /* Минимизатор после всех преобразований */
+    .pipe(csso()) /* Минимизатор CSS после всех преобразований */
     .pipe(rename("style.min.css")) /* Переименовываем файл после минимизации */
-    .pipe(sourcemap.write("."))
+    .pipe(sourcemap.write(".")) /* Записываем все изменения для отображения в инструментах разработчика */
     .pipe(gulp.dest("build/css")) /* Сохраняем получившийся контент в папку css */
 });
 
@@ -41,7 +41,7 @@ gulp.task("server", function () { /* Запускаем сервер и слеж
     cors: true,
     ui: false
   });
-/*  Вотчер для слежения ... */
+/*  Вотчеры для слежения ... */
   gulp.watch("source/less/**/*.less", gulp.series("css", "refresh")); /* ...за .less в любой подпапке и при изменении запусти gulp */
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh")); /* При добавлении svg-файла создаем спрайт и прогоняем posthtml */
   gulp.watch("source/*.html", gulp.series("html", "refresh")); /*  ... за .html файлами и прогони posthtml */
@@ -51,21 +51,20 @@ gulp.task("images", function () { /* Оптимизируем изображен
   return gulp.src("source/img/**/*.{png,jpg,svg}") /* Описываем контент с которым будем работать */
     .pipe(imagemin([
       imagemin.mozjpeg({quality: 100, progressive: true}) /* Оптимизируем изображение сжатием 1-7 с кол-во прогонов от 1-240 */
-
     ]))
-    .pipe(gulp.dest("source/img")); /* Сохраняем получившийся контент в папку css */
+    .pipe(gulp.dest("build/img")); /* Сохраняем получившийся контент в папку img */
 });
 
 gulp.task("webp", function () { /* Конвертируем jpeg, png в webp */
   return gulp.src("source/img/**/*.{png, jpg}")
     .pipe(webp({quality: 90})) /* Устанавливаем необходимое качество сжатия */
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("sprite", function () { /* Собираем спрайт из файлов icon-... */
   return gulp.src("source/img/icon-*.svg")
     .pipe(svgstore({ /* Передаем выбранные svg в svgstore */
-      inLineSvg: true
+      inLineSvg: true /* Сообщаем о том, что будем инлайнить в html файл */
     }))
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("build/img")); /* В папку build складываем */
@@ -90,7 +89,7 @@ gulp.task("copy", function () { /* Копируем файлы в отдельн
     "source/js/**",
     "source/*.ico"
   ], {
-    base: "source" /* Укажем корень для копирования папку source */
+    base: "source" /* Укажем корень для копирования папку source с подпапками */
   })
   .pipe(gulp.dest("build"));
 });
